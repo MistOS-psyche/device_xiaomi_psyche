89c4cb8cbf9528594fa7a2879ffc98b880c5732e
Verevka <verevka86@gmail.com>
2025-07-31 14:41:59 +0300

    psyche: Fix Miui Camera Repository

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-$(call inherit-product-if-exists, device/xiaomi/camera-psyche/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Dolby Support
 TARGET_USES_MIUI_DOLBY := true
